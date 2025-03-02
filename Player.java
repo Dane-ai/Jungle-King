aada82acda90826e2075a226d0cd5d697fd6e5e9
JethroArcher <161992261+JethroArcher@users.noreply.github.com>
2025-03-02 22:02:56 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,17 @@
 package mp;
 
 public class Player {
+  private string name;
+  private boolean isPlayerOne;
 
+  public Player ( string name, private boolean isPlayerOne){
+  this.name = name;
+  this.isPlayerOne = isPlayerOne;
+  
+}
+public string getName(){
+  return name;
+}
+public boolean getisPlayerOne(){
+  return isPlayerOne;
 }
