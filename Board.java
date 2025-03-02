745ae62cbb30ed31ad2ebad4e2e040a92a4b6586
Dane-ai <danielle_kate_sosuntong@dlsu.edu.ph>
2025-03-02 21:13:04 +0800

    Create Board.java
    The Board for the game

---
+++ Board.java
@@ -0,0 +1,24 @@
+package mp;
+
+import java.util.ArrayList;
+
+public class Board {
+	private ArrayList<Tile> grid;
+	private int x,y;
+	
+	public Board()
+	{
+		this.grid = new ArrayList<>();
+	}
+	
+	public void displayBoard()
+	{
+		
+	}
+	
+	public void updateBoard()
+	{
+		
+	}
+	
+}
