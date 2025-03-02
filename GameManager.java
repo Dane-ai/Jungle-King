a2380eb7d164de2d47bba8ed0ed8c0e0fa24d5dc
Dane-ai <danielle_kate_sosuntong@dlsu.edu.ph>
2025-03-02 20:55:58 +0800

    Create GameManager.java
    Driver for the MP

---
+++ GameManager.java
@@ -0,0 +1,32 @@
+package mp;
+
+import java.util.ArrayList;
+
+public class GameManager {
+
+	public static void main(String[] args)
+	{
+		
+		Board board = new Board();
+		Player turn;
+		ArrayList<Player> players = new ArrayList<>();
+		
+		
+		System.out.println(" Jungle King");
+		System.out.println(" 1 - Instructions");
+		System.out.println(" 2 - Start Game");
+			
+		public void startGame()
+		{
+			Player player1 = new Player("Player 1");
+			Player player2 = new Player("Player 2");
+			
+			players.add(player1);
+			players.add(player2);
+			
+		}
+	
+	
+	}
+
+}
