25cb173af8c6915e539a3e6c56cb4f03cf446918
JethroArcher <161992261+JethroArcher@users.noreply.github.com>
2025-03-03 13:45:15 +0800

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -28,9 +28,21 @@ public class GameManager {
 		System.out.println(" 2 - Start Game");
 
 		System.out.println("Enter Player 1's name");
-		Player player1 = new Player(scanner.nextLine(), true);
+		Player player1 = new Player(scan.nextLine(), true);
 		System.out.println("Enter Player 2's name");
-		Player player2 = new Player(scanner.nextLine(), false);
+		Player player2 = new Player(scan.nextLine(), false);
+
+		Collection.shuffle(animalType);
+		System.out.println("Animals are shuffled, choose one to determine who will go first");
+		System.out.println("Enter Player 1's choice");
+		int ch1 = scan.nextInt() - 1;
+		System.out.println("Enter Player 2's choice");
+		int ch2 = scan.nextInt() - 1;
+
+		
+
+
+
 
 		
 		
