bc0bac509aadaae08ba353588c1a694dcd75490b
JethroArcher <161992261+JethroArcher@users.noreply.github.com>
2025-03-03 10:41:28 +0800

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,8 +1,24 @@
 package mp;
 
 public class Tile {
-	private int xPos, yPos;
-	private String tileType;
-	
+	//private int xPos, yPos;
+	//private String tileType;
 
+	private boolean water;
+	private boolean trap;
+
+	public Tile(boolean water, boolean trap;){
+		this.Water = Water;
+		this.Trap = Trap;
+
+	}
+	public boolean water(){
+		return water;
+		
+	}
+	public boolean trap(){
+		return trap;
+
+		
+	}
 }
