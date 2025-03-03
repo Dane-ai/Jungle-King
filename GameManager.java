8a9bef586b7fccaa91f30c920183bfceaf74607e
JethroArcher <161992261+JethroArcher@users.noreply.github.com>
2025-03-03 12:08:15 +0800

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -2,6 +2,7 @@ package mp;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections
 
 public class GameManager {
 
@@ -9,20 +10,29 @@ public class GameManager {
 	{
 		
 		Board board = new Board();
-		ArrayList<Player> players = new ArrayList<>();
 		Scanner scan = new Scanner(System.in);
-		boolean start = true;
 		boolean player1Turn = true;
+		ArrayList<AnimalPiece> animalType = new ArrayList<>();
+		animalType.add(new Elephant(0, 0, true)); //position 0, 0 will fix later
+		animalType.add(new Lion(0, 0, true));
+		animalType.add(new Tiger(0, 0, true));
+		animalType.add(new Leopard(0, 0, true));
+		animalType.add(new Wolf(0, 0, true)); //position 0, 0 will fix later
+		animalType.add(new Dog(0, 0, true));
+		animalType.add(new Cat(0, 0, true));
+		animalType.add(new Rat(0, 0, true));
+		
 		
 		System.out.println(" Jungle King");
-		System.out.println(" 1 - Instructions");
+		System.out.println(" 1 - Instructions: ");
 		System.out.println(" 2 - Start Game");
 
 		System.out.println("Enter Player 1's name");
 		Player player1 = new Player(scanner.nextLine(), true);
 		System.out.println("Enter Player 2's name");
 		Player player2 = new Player(scanner.nextLine(), false);
-		while (start){
+
+		
 		
 
 			
